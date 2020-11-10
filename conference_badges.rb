be9198e22ab2f4e2ff344c15714e069e9bf16e02
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2020-11-10 18:04:14 -0500

    commit tree 5953b5fdc953521acbb46678d5a70668f4011cfb

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array) 
+    array.collect do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(speakers)
+    speakers.each_with_index.map do |speaker, index|
+        "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+    end
+end
+
+def printer(speakers)
+    badge_array = batch_badge_creator(speakers)
+    badge_array.each do |badge|
+        puts badge
+    end
+    room_array = assign_rooms(speakers)
+    room_array.each do |rooms|
+        puts rooms
+    end
+end
